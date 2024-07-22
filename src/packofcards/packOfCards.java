96fc5a9f62d56a7f50d60ba734f54d875de5efa3
ik <jeongik11029@naver.com>
2024-07-22 21:31:17 +0900

    cards의 인덱스 번호를 개별적으로 사용할 수 있게 처리. 이중for문 그리고 인덱스 번호가 끝나기 전에 return하지 않도록 한다.

--- src/packofcards/packOfCards.java
+++ src/packofcards/packOfCards.java
@@ -5,16 +5,24 @@ public class packOfCards {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
 
         for (int i = 0; i < goal.length; i++) {
+            System.out.println("goal[i] = " + goal[i]);
+            for (int j = 0; j < goal.length; j++) {
 
-            if ((cards1.length - 1) >= i && cards1[i].equals(goal[i])) {
-                continue;
+                //2. cards1과 cards2의 배열길이가 넘어가지 않게 처리.
+                if ((cards1.length - 1) >= j && cards1[j].equals(goal[i])) {
+                    break;
+                }
+
+                if ((cards2.length - 1) >= j && cards2[j].equals(goal[i])) {
+                    break;
+                }
+
+                if (j < goal.length) {
+                    continue;
+                }
+
+                return "No";
             }
-
-            if ((cards2.length - 1) >= i && cards2[i].equals(goal[i])) {
-                continue;
-            }
-
-            return "No";
         }
 
         return "Yes";
