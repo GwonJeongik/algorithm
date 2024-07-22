8a03600708eae791880b276db10c7aad2fd28fc8
ik <jeongik11029@naver.com>
2024-07-22 21:08:31 +0900

    cards1과 cards2의 배열길이가 넘어가지 않게 처리.

--- src/packofcards/packOfCards.java
+++ src/packofcards/packOfCards.java
@@ -6,12 +6,11 @@ public class packOfCards {
 
         for (int i = 0; i < goal.length; i++) {
 
-
-            if (cards1[i].equals(goal[i])) {
+            if ((cards1.length - 1) >= i && cards1[i].equals(goal[i])) {
                 continue;
             }
 
-            if (cards2[i].equals(goal[i])) {
+            if ((cards2.length - 1) >= i && cards2[i].equals(goal[i])) {
                 continue;
             }
 
